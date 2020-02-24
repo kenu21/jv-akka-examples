df5c486ed766d02aca0937eb51617179577445b3
Iurii Keniu <kenu21@gmail.com>
2020-02-24 13:48:22 +0200

    added chatroomobject example

---
+++ src/main/java/chatroomobject/Main.java
@@ -0,0 +1,27 @@
+package chatroomobject;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.Terminated;
+import akka.actor.typed.javadsl.Behaviors;
+
+public class Main {
+    public static Behavior<Void> create() {
+        return Behaviors.setup(
+                context -> {
+                    ActorRef<ChatRoom.RoomCommand> chatRoom = context.spawn(ChatRoom.create(), "chatRoom");
+                    ActorRef<ChatRoom.SessionEvent> gabbler = context.spawn(Gabbler.create(), "gabbler");
+                    context.watch(gabbler);
+                    chatRoom.tell(new ChatRoom.GetSession("ol’ Gabbler", gabbler));
+
+                    return Behaviors.receive(Void.class)
+                            .onSignal(Terminated.class, sig -> Behaviors.stopped())
+                            .build();
+                });
+    }
+
+    public static void main(String[] args) {
+        ActorSystem.create(Main.create(), "ChatRoomDemo");
+    }
+}
